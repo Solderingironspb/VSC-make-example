d7fcc05559f5730f80975155fae01ffc3a720760
unknown <olegdizzvolkov@yandex.ru>
2025-02-28 01:09:17 +0300

    update

--- Example_STM32F103_VSCode/.vscode/ps_scripts/Makefile_update_json.ps1
+++ Example_STM32F103_VSCode/.vscode/ps_scripts/Makefile_update_json.ps1
@@ -7,25 +7,136 @@
 #   RuTube: https://rutube.ru/channel/36234184/
 #   Яндекс Дзен: https://dzen.ru/id/622208eed2eb4c6d0cd16749
 
+$DELAY = 500
+
 # Получаем аргумент (имя целевого проекта)
-$TARGET = $args[0]
-$BUILD_DIR = $args[1]
+$TARGET = $args[0]  #Название проекта
+$BUILD_DIR = $args[1]   #Папка, в которой будут складываться файлы для дебага
+$C_INCLUDES = $args[2] #Папки через пробел, в которых находятся *.h файлы
+$C_DEFS = $args[3] #Defines GNU C
+$GNU_TOOLCHAIN_GCC_PATH = $args[4] #arm-none-eabi-gcc.exe
+$updatedArgs5 = $args[5] -replace "update-json", "" #костыль, но пусть так...я не знаю почему сюда затисалось это :)
+$CFLAGS = $updatedArgs5 #Все аргументы CFLAGS
 
-#Write-Host "Читаем файл c_cpp_properties.json, заменяем строку с name" -ForegroundColor Magenta
+
+#Функция для преобразования аргументов, записанных через пробел в строку с соответствующими параметрами
+function Parser_args {
+    param (
+        [string]$inputString, # Входная строка с путями
+        [string]$Prefix = "`${workspaceFolder}/", # Строка, добавляемая в начало каждого пути (по умолчанию "${workspaceFolder}/")
+        [string]$Suffix = "/**"  # Строка, добавляемая в конец каждого пути (по умолчанию "/**")
+    )
+    # Разделяем строку на отдельные пути по пробелам и удаляем пустые элементы
+    $paths = $inputString -split ' ' | Where-Object { $_ -ne '' }
+    # Преобразуем каждый путь в нужный формат
+    $transformedPaths = $paths | ForEach-Object {
+        # Добавляем префикс, путь и суффикс, затем заключаем в кавычки
+        "`"$Prefix$_$Suffix`""
+    }
+    # Объединяем преобразованные пути в одну строку с разделителем ", "
+    $result = $transformedPaths -join ', '
+    # Возвращаем результат
+    return $result
+}
+
+# Функция замены массива параметров, когда они заключены в квадратные скобки []
+function Update_Json_Array_of_parameters {
+    param (
+        [string]$JsonFilePath, # Путь к JSON-файлу
+        [string]$NewValue, # Новая строка для вставки
+        [string]$ParameterName # Название параметра (например, "includePath")
+    )
+    # Проверка существования файла
+    if (-not (Test-Path -Path $JsonFilePath)) {
+        Write-Host "Файл '$JsonFilePath' не найден." -ForegroundColor Red
+        return
+    }
+    try {
+        # Чтение JSON-файла
+        $jsonContent = Get-Content -Path $JsonFilePath -Raw -Encoding utf8
+        # Регулярное выражение для поиска и замены значения параметра
+        $pattern = "`"$ParameterName`":\s*\[[^\]]*\]"
+        $updatedJsonContent = $jsonContent -replace $pattern, "`"$ParameterName`": [$NewValue]"
+        # Сохранение изменённого JSON обратно в файл
+        Start-Sleep -Milliseconds $DELAY
+        Set-Content -Path $JsonFilePath -Value $updatedJsonContent -Encoding utf8 -NoNewline 
+        Write-Host "Success" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Error" -ForegroundColor Red
+    }
+}
+
+# Функция замены параметра
+function Update_Json_Parameter {
+    param (
+        [string]$JsonFilePath, # Путь к JSON-файлу
+        [string]$NewValue, # Новая строка для вставки
+        [string]$ParameterName # Название параметра (например, "name")
+    )
+    # Проверка существования файла
+    if (-not (Test-Path -Path $JsonFilePath)) {
+        Write-Host "Файл '$JsonFilePath' не найден." -ForegroundColor Red
+        return
+    }
+    try {
+        # Чтение JSON-файла
+        $jsonContent = Get-Content -Path $JsonFilePath -Raw -Encoding utf8
+        # Регулярное выражение для поиска и замены значения параметра
+        $pattern = "`"$ParameterName`":\s*`"[^`"]*`""
+        $updatedJsonContent = $jsonContent -replace $pattern, "`"$ParameterName`": `"$NewValue`""
+        # Сохранение изменённого JSON обратно в файл
+        Start-Sleep -Milliseconds $DELAY
+        Set-Content -Path $JsonFilePath -Value $updatedJsonContent -Encoding utf8 -NoNewline
+        Write-Host "Success" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Error" -ForegroundColor Red
+    }
+}
+
+############################################ Читаем и правим файл c_cpp_properties.json ############################################
 # Читаем текущее значение поля "name" из файла
-$fileContent = Get-Content .vscode/c_cpp_properties.json -Raw -Encoding utf8
-$oldName = ($fileContent | Select-String -Pattern '"name": "(.*?)"' -AllMatches).Matches.Groups[1].Value
-Write-Host "file c_cpp_properties.json: " -ForegroundColor DarkYellow -NoNewline  
-Write-Host "string replacement 'name: $oldname' -> 'name: $TARGET' " -ForegroundColor Green
-(Get-Content .vscode/c_cpp_properties.json) -replace '"name": ".*"', ('"name": "' + $TARGET + '"') | Set-Content .vscode/c_cpp_properties.json
+$C_CPP_PROPERTIES_PATH = ".vscode/c_cpp_properties.json"
+Write-Host "file $C_CPP_PROPERTIES_PATH`:" -ForegroundColor DarkYellow
+
+# Преобразуем TARGET в формат, удобоваримыя для c_cpp_properties.json
+Write-Host "Makefile TARGET = $TARGET -> `"name`": `"$TARGET`" " -ForegroundColor White
+Update_Json_Parameter -JsonFilePath $C_CPP_PROPERTIES_PATH -NewValue $TARGET -ParameterName "name"  
+
+#Преобразуем $C_INCLUDES в формат, удобоваримый для c_cpp_properties.json
+$formattedPaths = Parser_args -inputString $C_INCLUDES
+Write-Host "Makefile C_INCLUDES = $C_INCLUDES -> 'includePath: [$formattedPaths]' " -ForegroundColor White
+Update_Json_Array_of_parameters -JsonFilePath $C_CPP_PROPERTIES_PATH -NewValue "$formattedPaths" -ParameterName "includePath"
+
+#Преобразуем $C_DEFS в формат, удобоваримый для c_cpp_properties.json
+$formattedDefs = Parser_args -inputString $C_DEFS -Prefix "" -Suffix ""
+Write-Host "Makefile C_DEFS = $C_DEFS -> 'defines: [$formattedDefs]' " -ForegroundColor White
+Update_Json_Array_of_parameters -JsonFilePath $C_CPP_PROPERTIES_PATH -NewValue "$formattedDefs" -ParameterName "defines" 
+
+# Преобразуем $GNU_TOOLCHAIN_GCC_PATH в формат, удобоваримыя для c_cpp_properties.json
+Write-Host "Makefile GNU_TOOLCHAIN_GCC_PATH = $GNU_TOOLCHAIN_GCC_PATH -> `"compilerPath`": `"$GNU_TOOLCHAIN_GCC_PATH`" " -ForegroundColor White
+Update_Json_Parameter -JsonFilePath $C_CPP_PROPERTIES_PATH -NewValue $GNU_TOOLCHAIN_GCC_PATH -ParameterName "compilerPath"
+
+#Преобразуем $CFLAGS в формат, удобоваримый для c_cpp_properties.json
+$formattedCFLAGS = Parser_args -inputString $CFLAGS -Prefix "" -Suffix ""
+Write-Host "Makefile CFLAGS = $CFLAGS -> 'compilerArgs: [$formattedCFLAGS]' " -ForegroundColor White
+Update_Json_Array_of_parameters -JsonFilePath $C_CPP_PROPERTIES_PATH -NewValue "$formattedCFLAGS" -ParameterName "compilerArgs" 
+
+############################################ Читаем и правим файл c_cpp_properties.json ############################################
 
 
-# Читаем файл launch.json, заменяем строку с executable
-$fileContent = Get-Content .vscode/launch.json -Raw -Encoding utf8
-$oldName = ($fileContent | Select-String -Pattern '"executable": "(.*?)"' -AllMatches).Matches.Groups[1].Value
-Write-Host "file launch.json: " -ForegroundColor DarkYellow -NoNewline  
-Write-Host "string replacement 'executable: .$oldname' -> 'executable: .$BUILD_DIR/$TARGET.elf' " -ForegroundColor Green
-(Get-Content .vscode/launch.json) -replace '"executable": ".*"', ('"executable": "'+ $BUILD_DIR + '/' + $TARGET + '.elf' + '"') | Set-Content .vscode/launch.json
+################################################# Читаем и правим файл launch.json #################################################
+# Читаем файл launch.json
+$LAUNCH_PATH = ".vscode/launch.json"
+# Заменяем текущее значение поля "executable" из файла
+# Преобразуем TARGET/BUILD в формат, удобоваримыя для launch.json
+Write-Host "file $LAUNCH_PATH`: " -ForegroundColor DarkYellow 
+Write-Host "Makefile BUILD_DIR/TARGET = $BUILD_DIR/$TARGET -> `"executable`": `"$BUILD_DIR/$TARGET.elf`" " -ForegroundColor White
+Update_Json_Parameter -JsonFilePath $LAUNCH_PATH -NewValue $BUILD_DIR/$TARGET -ParameterName "executable"
+
+Write-Host "string replacement 'executable: .$oldname' -> 'executable: $BUILD_DIR/$TARGET.elf' " -ForegroundColor Green
+(Get-Content .vscode/launch.json -Raw -Encoding utf8) -replace '"executable": ".*"', ('"executable": "' + $BUILD_DIR + '/' + $TARGET + '.elf' + '"') | Out-File .vscode/launch.json -Encoding utf8
 
 # Читаем файл tasks.json, заменяем строку с .elf и сохраняем изменения
 Write-Host "file tasks.json: " -ForegroundColor DarkYellow -NoNewline
