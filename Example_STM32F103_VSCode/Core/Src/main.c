3295dad2b654ff243d0d71d100c2075d8a47def2
unknown <olegdizzvolkov@yandex.ru>
2025-03-11 21:26:06 +0300

    update 11.03.2025

--- Example_STM32F103_VSCode/Core/Src/main.c
+++ Example_STM32F103_VSCode/Core/Src/main.c
@@ -7,10 +7,10 @@ int main(void) {
     CMSIS_Debug_init();
     CMSIS_RCC_SystemClock_72MHz();
     CMSIS_SysTick_Timer_init();
-    CMSIS_USART1_Init(); //115200 8N1
+    CMSIS_USART1_Init();  // 115200 8N1
     CMSIS_GPIO_init(GPIOC, 13, GPIO_GENERAL_PURPOSE_OUTPUT, GPIO_OUTPUT_PUSH_PULL, GPIO_SPEED_50_MHZ);
-    
 
+   
     while (1) {
         Counter++;
         sprintf((char*)Text, "Привет мир!\r\nСчетчик досчитал до %ld\r\n", Counter);
